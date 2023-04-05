0c1c07db0cb5df800bc7c5102bf76e01a3f928c4
Jonathan Torres <jonathanlt68@gmail.com>
2023-04-05 12:50:50 -0300

    Updatev4
    Finalizando a estilização dos ingredientes e imagens das comidas da pág inicial

--- src/components/foodlist/index.js
+++ src/components/foodlist/index.js
@@ -1,11 +1,21 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 
+import { LinearGradient } from 'expo-linear-gradient'
+
 export function FoodList({ data }){
     return(
-        <TouchableOpacity style={styles.container}>
+        <TouchableOpacity activeOpacity={0.9} style={styles.container}>
             <Image
                 source={{ uri: data.cover }}
                 style={styles.cover}
+            />
+            <View style={styles.info}>
+                <Text style={styles.name}>{data.name}</Text>
+                <Text style={styles.description}>{data.total_ingredients} ingredientes | {data.time} min </Text>
+            </View>
+            <LinearGradient
+                style={styles.gradient}
+                colors={['transparent', 'rgba(0,0,0, 0.70)', 'rgba(0,0,0, 0.95)']}
             />
         </TouchableOpacity>
     )
@@ -18,6 +28,30 @@ const styles = StyleSheet.create({
     cover:{
         width: '100%',
         height: 200,
-
+        borderRadius: 14,
+    },
+    info:{
+        position: 'absolute',
+        bottom: 14,
+        left: 14,
+        zIndex: 99
+    },
+    name:{
+        fontSize: 18,
+        color: '#FFF',
+        fontWeight: 'bold',
+    },
+    description:{
+        color:'#FFF'
+    },
+    gradient:{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: '55%',
+        borderRadius: 14,
+        zIndex: 1,
+        backgroundColor: 'transparent',
     }
 })
