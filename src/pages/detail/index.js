bb1c564416451f17f109204cb9be7bcdadffea21
Jonathan Torres <jonathanlt68@gmail.com>
2023-04-06 10:04:58 -0300

    Updatev5

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,7 @@
 import { useLayoutEffect } from 'react'
-import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Pressable, ScrollView, Image, } from 'react-native'
 import {  useRoute, useNavigation } from '@react-navigation/native'
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, AntDesign } from '@expo/vector-icons'
 
 
 export function Detail(){
@@ -27,15 +27,43 @@ export function Detail(){
     }, [navigation, route.params?.data])
 
     return(
-        <View style={styles.container}>
-            <Text>Página Detalhes da Receita!!!</Text>
-            <Text>{route.params?.data.name}</Text>
-        </View>
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+            <Pressable>
+                <View style={styles.playIcon}>
+                    <AntDesign name="playcircleo" size={50} color="#FAFAFA"  />
+                </View>
+                <Image
+                    source={{ uri: route.params?.data.cover}}
+                    style={styles.cover}
+                />
+            </Pressable>
+
+            <View style={styles.header.Details}>
+
+            </View>
+
+
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: 'blue',
+        backgroundColor: '#F3f9ff',
+        paddingTop: 14,
+        paddingEnd: 14,
+        paddingStart: 14
+    },
+    cover:{
+        height: 200,
+        borderRadius: 14,
+        width: '100%',
+    },
+    playIcon:{
+        position: 'absolute',
+        zIndex: 9,
+        top: 0, left: 0, right: 0, bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 })
