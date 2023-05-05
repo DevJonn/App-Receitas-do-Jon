d4eb9440b8bd7ddfa33cde63ba386bb8aea52b29
Jonathan Torres <jonathanlt68@gmail.com>
2023-05-05 10:42:52 -0300

    Updatev7

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react' //useState busca o status, useEffect busca a lista.
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { getFavorites } from '../../utils/storage'
 
 export function Favorites(){
+    const [receipes, setReceipes] = useState([]);
+
+    useEffect(()=> {
+
+        async function getReceipes(){
+            const result = await getFavorites('@appreceitas');
+        }
+
+        getReceipes();
+
+    }, [])
+
     return(
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Receitas Favoritas</Text>
+
+        {receipes.length == 0 && (
+            <Text>Você ainda não tem nenhuma receita salva.</Text>
+        )}
+
         </SafeAreaView>
     )
 }
