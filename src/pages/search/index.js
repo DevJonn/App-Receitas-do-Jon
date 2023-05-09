81a0c6ba9e11708c3114297e014941a1ba3192c7
Jonathan Torres <jonathanlt68@gmail.com>
2023-05-09 11:18:53 -0300

    Updatev8

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,15 +1,36 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { useState, useEffect } from 'react'
+import { useRoute } from '@react-navigation/native'
+import api from '../../services/api'
+
 
 export function Search(){
+    const route = useRoute();
+
+    useEffect(() => {
+        async function  fetchReceipes(){
+            const response = await api.get(`/foods?name_like=${route.params?.name}`)
+            
+        }
+
+        fetchReceipes();
+
+    }, [route.params?.name])
+
+
     return(
         <View style={styles.container}>
-            <Text>Página Buscar!!!</Text>
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: 'green',
+        flex: 1,
+        backgroundColor: '#f3f9ff',
+        paddingStart: 14,
+        paddingEnd: 14,
+        paddingTop: 14,
     }
 })
